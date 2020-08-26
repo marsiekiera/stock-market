78f16f3c775112f987e2b96222f28cb9b2deb2a6
marsiekiera <hatchet17@gmail.com>
2020-08-26 22:14:05 +0200

    create file

---
+++ createdb.py
@@ -0,0 +1,10 @@
+from cs50 import SQL
+import sqlite3
+
+conn = sqlite3.connect('finance.db')
+
+db = SQL("sqlite:///finance.db")
+
+db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC);")
+db.execute("CREATE TABLE history (id INTEGER PRIMARY KEY, user_id INTEGER, stock_symbol TEXT NOT NULL, stock_name TEXT, shares INTEGER, price REAL, total REAL, date TEXT);")
+db.execute("CREATE TABLE stocks (id INTEGER PRIMARY KEY, user_id INTEGER, stock_symbol TEXT NOT NULL, stock_name TEXT, shares INTEGER, price REAL, total REAL);")
