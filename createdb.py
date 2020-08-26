aa1f899fdcbb598449677a68500bb7e00742fdf9
marsiekiera <hatchet17@gmail.com>
2020-08-26 22:42:40 +0200

    update

--- createdb.py
+++ createdb.py
@@ -5,6 +5,6 @@ conn = sqlite3.connect('finance.db')
 
 db = SQL("sqlite:///finance.db")
 
-db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC);")
+db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC DEFAULT 50);")
 db.execute("CREATE TABLE history (id INTEGER PRIMARY KEY, user_id INTEGER, stock_symbol TEXT NOT NULL, stock_name TEXT, shares INTEGER, price REAL, total REAL, date TEXT);")
 db.execute("CREATE TABLE stocks (id INTEGER PRIMARY KEY, user_id INTEGER, stock_symbol TEXT NOT NULL, stock_name TEXT, shares INTEGER, price REAL, total REAL);")
