ce6aab778798a674645e65e3112906044fc27115
marsiekiera <hatchet17@gmail.com>
2020-08-26 21:48:06 +0200

    create file

--- application.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, redirect, render_template, request
-
-app = Flask(__name__)
-
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-@app.route("/")
-def index():
-    return render_template("index.html")
